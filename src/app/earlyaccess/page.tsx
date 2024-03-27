56ae56d7e7e44e2fe50da4837aa83d6a198aa4cf
yeshealer <yeshealer@gmail.com>
2024-03-28 01:31:03 +0900

    update

--- src/app/earlyaccess/page.tsx
+++ src/app/earlyaccess/page.tsx
@@ -9,21 +9,30 @@ const SignupPage = () => {
       <Head>
         <meta charSet="utf-8" />
         <title>BULLZI</title>
-        <meta property="og:title" name="title" content="BULLZI" key="title" />
         <meta
-          property="og:description"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          property="twitter:title"
+          name="title"
+          content="BULLZI"
+          key="title"
+        />
+        <meta
+          property="twitter:description"
           name="description"
           content="#BULLZIMOVE YOURE STILL EARLY"
           key={"description"}
         />
         <meta
-          property="og:image"
+          property="twitter:image"
           name="image"
           content="https://res.cloudinary.com/pauloski/image/upload/v1710952504/pvd9fjruyjtuvnnjen0h.png"
         />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="BULLZI" />
-        <meta property="og:locale" content="en_US" />
+        <meta property="twitter:type" content="website" />
+        <meta property="twitter:site_name" content="BULLZI" />
+        <meta property="twitter:locale" content="en_US" />
       </Head>
       <LandingPage />;
     </div>
