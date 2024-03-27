8c33e5639a68260a2259c9ffd8cf06163997de5d
yeshealer <yeshealer@gmail.com>
2024-03-28 02:42:32 +0900

    fix issue

--- src/components/AdminPanelSection/index.tsx
+++ src/components/AdminPanelSection/index.tsx
@@ -7,7 +7,7 @@ export default function AdminPanelSection() {
 
   useEffect(() => {
     fetchAllInfo();
-  }, [fetchAllInfo]);
+  }, []);
 
   return (
     <div className="w-screen h-screen p-10 bg-green-400/50">
