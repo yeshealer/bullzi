e9c0b45cf55f02a5ea08d051ecc84062cee4ebdd
yeshealer <yeshealer@gmail.com>
2024-03-28 02:15:40 +0900

    udpate

--- src/app/earlyaccess/page.tsx
+++ src/app/earlyaccess/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import LandingPage from "@/components/LandingPage";
 import { Metadata } from "next";
 
@@ -20,39 +18,7 @@ export const metadata: Metadata = {
 };
 
 const SignupPage = () => {
-  return (
-    <div>
-      {/* <Head>
-        <meta charSet="utf-8" />
-        <title>BULLZI</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta
-          property="twitter:title"
-          name="title"
-          content="BULLZI"
-          key="title"
-        />
-        <meta
-          property="twitter:description"
-          name="description"
-          content="#BULLZIMOVE YOURE STILL EARLY"
-          key={"description"}
-        />
-        <meta
-          property="twitter:image"
-          name="image"
-          content="https://res.cloudinary.com/pauloski/image/upload/v1710952504/pvd9fjruyjtuvnnjen0h.png"
-        />
-        <meta property="twitter:type" content="website" />
-        <meta property="twitter:site_name" content="BULLZI" />
-        <meta property="twitter:locale" content="en_US" />
-      </Head> */}
-      <LandingPage />;
-    </div>
-  );
+  return <LandingPage />;
 };
 
 export default SignupPage;
